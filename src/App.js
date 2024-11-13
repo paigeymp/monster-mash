d6ea47f7d8d527788c813d680a4b2328176adc36
Paige Powell <117302610+paigeymp@users.noreply.github.com>
2024-11-13 08:17:14 -0500

    mapping monsters testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
+import { Component } from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: "Linda",
+        },
+        {
+          name: "Frank",
+        },
+        {
+          name: "Jacky",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          {this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>;
+          })}
+          }
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
