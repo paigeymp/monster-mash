68e5d5ed28dd7f3dedbc04515d00d665cf5478da
Paige Powell <117302610+paigeymp@users.noreply.github.com>
2024-11-21 06:40:20 -0700

    Added search input

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log("constructor");
   }
 
   componentDidMount() {
+    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -28,8 +30,25 @@ class App extends Component {
   }
 
   render() {
+    console.log("render");
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
         <h1>
           {this.state.monsters.map((monster) => {
             return (
